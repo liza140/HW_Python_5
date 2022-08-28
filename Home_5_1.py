6f1bb8b6188a2d1cf91d3397f5715362409b0520
liza140 <liza140@mail.ru>
2022-08-28 15:34:28 +0300

    Элементы больше 5

---
+++ Home_5_1.py
@@ -0,0 +1,9 @@
+# Задача 1. Задайте список случайных чисел от 1 до 10, выведите все элементы больше 5. Используйте для решения лямбда-функцию.
+# 2, 3, 4, 6, 7, 8 -> 6, 7, 8
+
+import random
+
+my_list = list(random.randint(1,10) for i in range(10))
+print(my_list)
+my_list = list(filter(lambda x: x>5, my_list))
+print(my_list)
