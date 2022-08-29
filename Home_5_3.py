c02fd491f6ae279fb28bccf7d94e327e4c86c6fa
liza140 <liza140@mail.ru>
2022-08-29 22:51:09 +0300

    Количество повторяющихся элементов

--- Home_5_3.py
+++ Home_5_3.py
@@ -8,4 +8,11 @@ import random
 my_list = list(random.randint(1,10) for i in range(10))
 print(my_list)
 
+count = 0
+for i in my_list:
+    print(f"{i} встречается в списке {my_list.count(i)} раз(-а)")
+    if my_list.count(i) > 1:
+        count +=1
 
+print("Количество совпадающих элементов: ", count)
+print("Список уникальных элементов: ", list(set(my_list)))
